5f55e79dbe519b4e49dc05803378967007b5d582
William Seymour <williamseymour123@googlemail.com>
2015-10-25 11:30:16 +0000

    Hotfix to well, um, fix card orders
    Addition of live/test modes did not make it to the config file ¬_¬

--- config-sample.php
+++ config-sample.php
@@ -4,8 +4,10 @@ return array(
   'username'    => 'db-user',
   'password'    => 'db-password',
   'dbname'      => 'db-name',
-  'api_public'  => 'stripe-api-public',
-  'api_private' => 'stripe-api-private',
+  'api_public_live'  => 'stripe-api-public',
+  'api_private_live' => 'stripe-api-private',
+  'api_public_test'  => 'stripe-api-public',
+  'api_private_test' => 'stripe-api-private',
   'cp_pass'     => 'missioncontrol-password',
   'cp_guid'     => 'some-guid',
   'session_path'=> '/path/to/sessions'
